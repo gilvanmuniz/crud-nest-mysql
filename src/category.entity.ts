376bc4a256d1f1bd6cd2a3398279f604416c118c
gilvanmuniz <camisetaecia@live.com>
2020-12-15 15:47:30 -0300

    subindo base

---
+++ src/category.entity.ts
@@ -0,0 +1,14 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Timestamp } from 'typeorm'
+
+@Entity()
+export class Category{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @CreateDateColumn({ type:'timestamp'})
+    created_at:Date;
+}
